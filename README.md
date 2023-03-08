6b96694f8aa8cc8f00c29d2bc836d75b94206ce7
caiooliveira01 <122302892+caiooliveira01@users.noreply.github.com>
2023-03-08 17:26:12 -0300

    Atualização

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
  
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
+    setlocale(LC_ALL,"Portuguese_Brazil");
+
     /* code */
     return 0;
 }
--- lista-01/ex01.c
+++ lista-01/ex01.c
@@ -1,12 +1,21 @@
+// Receber um número, calcular seu dobro e exibir o resultado. 
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
+    setlocale(LC_ALL,"Portuguese_Brazil");
+
     float x;
-    printf("Digite um nÃºmero: ");
+
+    printf("Digite um número: ");
     scanf("%f", &x);
+
     float n = x * 2;
-    printf("O dobro Ã© %6.2f", n);
+
+    printf("O dobro de %4.2f é: %4.2f", x, n);
+
     return 0;
 }
